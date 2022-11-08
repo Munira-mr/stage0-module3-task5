459111b0ab66b6cb68930186acaf7495b03aead6
munira-mr <munirarizaeva1@gmail.com>
2022-11-08 18:59:22 -0500

    Solved...

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds;
+        seconds=minutes*60;
+        System.out.println(minutes+" minutes= "+seconds+" seconds");
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor t=new TimeConvertor();
+        t.convert(3);
+
     }
 }
