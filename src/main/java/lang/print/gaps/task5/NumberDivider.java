6aa9d00196217f51059615eeeb74e7a0bdb704dc
Munira <79731097+Munira-mr@users.noreply.github.com>
2022-11-08 19:03:53 -0500

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        System.out.println(toBeDivided/5);
+        int result=toBeDivided/5;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
